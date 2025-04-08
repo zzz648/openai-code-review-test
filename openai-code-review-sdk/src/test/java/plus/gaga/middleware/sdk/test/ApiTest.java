8259f69f9dd1982c14d0f566d1d07fe2674dd63e
zengdepeng <648949599@qq.com>
2025-04-09 02:51:18 +0800

    feat:测试v1

--- openai-code-review-sdk/src/test/java/plus/gaga/middleware/sdk/test/ApiTest.java
+++ openai-code-review-sdk/src/test/java/plus/gaga/middleware/sdk/test/ApiTest.java
@@ -3,6 +3,10 @@ package plus.gaga.middleware.sdk.test;
 public class ApiTest {
 
     public static void main(String[] args) {
+        System.out.println("测试执行");
+        System.out.println("测试执行");
+        System.out.println("测试执行");
+        System.out.println("测试执行");
 
     }
 
